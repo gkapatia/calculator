6e824cae0dcfa7085bcdfbaf86459e993542c30d
Gaurav Kapatia <gkapatia@gmail.com>
2020-04-03 14:42:56 +0000

    Updates

---
+++ precache-manifest.682bcf6340fd0dbec3b5050726d31b49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6769b0e05d727d105416e9f3941395bc",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "907ccce90655e1fb9d46",
+    "url": "/calculator/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "907ccce90655e1fb9d46",
+    "url": "/calculator/static/js/main.c8d3e0ce.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
